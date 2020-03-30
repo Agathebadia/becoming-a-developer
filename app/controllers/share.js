a91c59eee8757462ed799af506ad55cac141f611
Agathe Badia <ab@honeypot.io>
2020-03-30 20:40:22 +0200

    Update radio buttons to toggle

--- app/controllers/share.js
+++ app/controllers/share.js
@@ -1,11 +1,5 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class ShareController extends Controller {
-  @tracked yesContact = false;
-  @action changeYesContent(bool) {
-    this.yesContact = bool
-  }
 }
 
