af694e344fe3844929894c3faec8aeeda9824321
Agathe Badia <ab@honeypot.io>
2020-05-25 16:55:59 +0200

    Add new story with img and change line height for easier reading

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ['purple-flower-round.jpg', 'dieter-pankratz.png', 'eyeglasses-round.jpg', 'men-camera-round.jpg', 'petaled-flowers-round.jpg', 'purple-abstract-round.jpg', 'purple-flower-round.jpg', 'splatter-round.jpg', 'seanb.jpg', 'favicon.ico'],
+      exclude: ['purple-flower-round.jpg', 'dieter-pankratz.png', 'eyeglasses-round.jpg', 'men-camera-round.jpg', 'petaled-flowers-round.jpg', 'purple-abstract-round.jpg', 'purple-flower-round.jpg', 'splatter-round.jpg', 'seanb.jpg', 'agatheb.png', 'favicon.ico'],
     }
   });
 
